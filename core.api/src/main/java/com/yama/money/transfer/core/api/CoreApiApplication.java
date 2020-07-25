8b683ec9db7343d6a6c161824d04bc7ad94cf607
Mohamed Homaid <mhomaid@gmail.com>
2020-07-25 03:00:34 -0400

    Added core api startup project based on spring boots with the following dependencies=devtools,web,session,security,oauth2-client,data-rest,actuator,mail,restdocs,okta,postgresql

---
+++ core.api/src/main/java/com/yama/money/transfer/core/api/CoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.yama.money.transfer.core.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApiApplication.class, args);
+	}
+
+}
